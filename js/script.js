324d1f206f197ec4f2b80e7f65f6075ed5df3d03
Kndarp Patel <kndarppatel@gmail.com>
2016-06-25 17:26:06 +0530

    Added Numeric Validation

--- js/script.js
+++ js/script.js
@@ -356,6 +356,8 @@ $(function(){
     (empty)?$('button#add').attr('disabled','disabled'): $('button#add').removeAttr('disabled');
   });
 
+  $('div.jsonTable').on('keydown', 'input', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||/65|67|86|88/.test(e.keyCode)&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
+
 
 
 });
